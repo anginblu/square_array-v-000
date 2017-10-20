e9aae6bb482d906c7a261b60dd4ab672659f5aa1
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 01:42:37 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,12 @@
 def square_array(array)
   counter = array.size
-    array.each do |number|
-        array << number ** 2
+    array.each |number| 
+      until counter == 0 
+        array << number **2 
         counter -= 1
-        unless counter > 0
-          break
         end
     end
-    counter = array.size / 2
-    print counter
+
 end
 
 square_array ([1, 2, 3])
