225d33e768077eb2ac388adedb0b8e81e1471341
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 01:15:59 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
   # your code here
     array.each do |number|
-        new_number = number ** 2
-        new_array << new_number
+        replace number with number ** 2
     end
 end
