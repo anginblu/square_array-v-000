58860ab2dfd777d1c6977051d7de67e5cba26217
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 01:34:58 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,11 +2,11 @@ def square_array(array)
   # your code here]
   counter = array.size
     array.each do |number|
-          array << number ** 2
-          counter -= 1
-          if counter =0
-            abort
-          end
+        array << number ** 2
+        counter -= 1
+        if counter =0
+          abort
         end
     end
+    counter = array.size / 2
 end
