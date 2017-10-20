e9b21bf331084c4b345a5896ae6f325440daef7a
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 01:24:48 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
     array.each do |number|
-        number = number ** 2
-        array = array.shift
+        array << number ** 2
+        array.delete (number)
     end
 end
